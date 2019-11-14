95a68e3bb817843968b1d64bb384e4dec306cf2c
Edward George <edward.george@maersk.com>
2019-11-14 12:09:38 +0100

    implement working yaml-strip-nulls binary

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # yaml-strip-nulls
+
+Quick and dirty program in Haskell to strip `null` properties from objects in YAML files.
+
+## Usage
+
+```
+$ yaml-strip-nulls path/to/file.yaml
+```
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
-
-import Lib
+import Data.Aeson (Value(..))
+import qualified Data.ByteString as BS
+import Data.Hashable
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as Map
+import Data.Yaml (decodeFileThrow, encode)
+import Options.Applicative
 
 main :: IO ()
-main = someFunc
+main = do
+  filename <- execParser $ info filenameP mempty
+  val <- decodeFileThrow filename
+  let stripped = stripNulls val
+  BS.putStr $ encode stripped
+
+filenameP :: Parser FilePath
+filenameP = strArgument (metavar "FILE" <> help "YAML file to load")
+
+stripNulls :: Value -> Value
+stripNulls (Object o) = Object $ stripInMap o
+stripNulls (Array a) = Array $ fmap stripNulls a
+stripNulls a = a
+
+stripInMap :: (Eq k, Hashable k) => HashMap k Value -> HashMap k Value
+stripInMap = Map.foldlWithKey' step mempty
+  where
+    step accum key val = case val of
+      Null -> accum
+      _ -> Map.insert key (stripNulls val) accum
